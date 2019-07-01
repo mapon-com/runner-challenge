8304075789ede846d331a6ae029cee8bbc719fea
MBriedis <mbriedis@gmail.com>
2019-07-01 17:11:44 +0300

    Asset url generation

--- src/functions.php
+++ src/functions.php
@@ -10,3 +10,12 @@ function route($route): string
 {
     return Bootstrap::$routeGenerator->generate($route);
 }
+
+/**
+ * @param string $url
+ * @return string
+ */
+function asset(string $url): string
+{
+    return Bootstrap::$routeGenerator->generate('index') . ltrim($url, '/');
+}
