4dd8a19c6907b2578dfd224ede8d2a5fe124f0de
MBriedis <mbriedis@printful.com>
2019-09-15 11:20:25 +0300

    Allows duplicate upload for now.

--- src/Services/ActivityService.php
+++ src/Services/ActivityService.php
@@ -45,7 +45,8 @@ class ActivityService
         $md5 = md5($content);
 
         if (R::findOne('files', 'md5 = ?', [$md5]) && !getenv('DEBUG') === 'true') {
-            throw new InvalidArgumentException('This activity has already been uploaded');
+            // TODO temporarily disable duplicate check so people can upload broken activities
+            // throw new InvalidArgumentException('This activity has already been uploaded');
         }
 
         $file = R::dispense('files');
